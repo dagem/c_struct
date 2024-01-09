e14e785a40797330196aff2d1e250f0f51138c70
dagem <dagem@dagem.net>
2024-01-09 06:28:01 -0500

    pop and peek

--- src/stack/stack.c
+++ src/stack/stack.c
@@ -31,3 +31,23 @@ void push(stack *st, void* data)
 	
 	st->head = tmp;
 }
+void* peek(stack* st)
+{
+	return st->head->data;
+}
+
+void* pop(stack* st)
+{
+	if(st->head->next == NULL)
+	{
+		void* tmp_data = st->head->data;
+		st = malloc(sizeof(stack));
+		return tmp_data;
+	}
+	else
+	{
+		void* tmp_data = st->head->data;
+		st->head = st->head->next;
+		return tmp_data;
+	}
+}
